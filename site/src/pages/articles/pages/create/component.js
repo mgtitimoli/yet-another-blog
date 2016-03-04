01dac7071f7a64b4f1eb2464deafe25023a8cc6d
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2016-03-04 11:02:27 -0300

    Integrated article-editor component

--- site/src/pages/articles/pages/create/component.js
+++ site/src/pages/articles/pages/create/component.js
@@ -1,15 +1,55 @@
-import React, { Component } from "react";
+import React, {
+    Component,
+    PropTypes
+} from "react";
 
-// import styles from "./styles.css";
+import ArticleEditor from "pages/articles/controls/article-editor";
+// import styles from "./component.css";
 
 export default class ArticlesCreatePage extends Component {
 
     static displayName = ArticlesCreatePage.name;
 
+    static propTypes = {
+        onCancel: PropTypes.func.isRequired,
+        onSave  : PropTypes.func.isRequired
+    };
+
+    componentWillMount() {
+
+        this.state = {
+            newArticle: {
+                title  : "",
+                content: ""
+            }
+        };
+    }
+
+    _renderArticleEditor() {
+
+        return (
+            <ArticleEditor
+                article={ this.state.newArticle }
+                onCancel={ this.props.onCancel }
+                onSave={ this.props.onSave }
+            />
+        );
+    }
+
+    _renderTitle() {
+
+        return (
+            <h1>Create Article</h1>
+        );
+    }
+
     render() {
 
         return (
-            <h1>ARTICLES CREATE!</h1>
+            <div>
+                { this._renderTitle() }
+                { this._renderArticleEditor() }
+            </div>
         );
     }
 }
