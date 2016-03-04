28b06bf331fe82e648c499e8c9d33497e7bf4d3b
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2016-03-04 10:54:10 -0300

    Added react-router-redux enhacer

--- site/src/lib/history.js
+++ site/src/lib/history.js
@@ -1,9 +1,20 @@
-import {
-    createHistory,
-    useBasename,
-    useQueries
-} from "history";
+import createBrowserHistory from "history/lib/createBrowserHistory";
+import { syncHistoryWithStore } from "react-router-redux";
+import { useRouterHistory } from " react-router";
 
-export default useBasename(useQueries(createHistory))({
+import store from "store";
+
+const browserHistoryWithBasename = useRouterHistory(createBrowserHistory)({
     basename: __config.basePath
 });
+
+// see: https://github.com/reactjs/react-router-redux/issues/301
+const options = {
+    selectLocationState: state => state.get("routing").toJS()
+};
+
+export default syncHistoryWithStore(
+    browserHistoryWithBasename,
+    store,
+    options
+);
