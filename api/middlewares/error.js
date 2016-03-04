74a07a64c3e7b65d776cf7a717f849347bc7ffe3
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2016-03-04 10:56:36 -0300

    Now error middleware uses error and create-response utils

--- api/middlewares/error.js
+++ api/middlewares/error.js
@@ -1,37 +1,29 @@
+import httpStatus from "http-status-codes";
+
+import createHttpError from "../lib/error/create-http";
+import createResponse from "../lib/create-response";
+
 function handleNotFound(req, res, next) {
 
-    const error = new Error("Not found");
-
-    error.httpStatus = 404;
-
-    next(error);
+    next(createHttpError(
+        httpStatus.NOT_FOUND
+    ));
 }
 
 function returnError(error, req, res) {
 
-    let httpStatus = error.httpStatus;
+    const responseError = error.httpStatusCode ?
+        error :
+        createHttpError(
+            httpStatus.INTERNAL_SERVER_ERROR
+        );
 
-    let code;
-    let message;
+    res.status(error.httpStatusCode);
 
-    if (httpStatus) {
-        code       = error.code;
-        message    = error.message;
-    }
-    else {
-        httpStatus = 500;
-        code       = 1000;
-        message    = "Internal error";
-    }
-
-    res.status(httpStatus);
-
-    res.json({
-        error: {
-            code,
-            message
-        }
-    });
+    res.json(createResponse(
+        undefined,
+        responseError
+    ));
 }
 
 export default [
