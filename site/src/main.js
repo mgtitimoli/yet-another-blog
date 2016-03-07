84776ad72e0cf62c6813125d14db60bd625cb970
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2016-03-07 12:31:49 -0300

    Added missing history import

--- site/src/main.js
+++ site/src/main.js
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom";
 import { Router } from "react-router-redux";
 
 import applicationRoute from "pages/application/route";
+import history from "lib/history";
 
 ReactDOM.render(
     <Router
