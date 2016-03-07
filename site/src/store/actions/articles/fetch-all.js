c4357941c1b4ddf5adde87bfe04f6fb9b5943a85
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2016-03-07 12:35:10 -0300

    Added articles fetch-all action

---
+++ site/src/store/actions/articles/fetch-all.js
@@ -0,0 +1,12 @@
+import articlesService from "services/articles";
+import createAsyncActionCreator from "../lib/create-async-action-creator";
+
+export function articlesFetchAll(articlesService) {
+
+    return articlesService.fetchAll();
+}
+
+export default createAsyncActionCreator(
+    "ARTICLES:FETCH_ALL",
+    articlesFetchAll.bind(undefined, articlesService)
+);
