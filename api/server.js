985975b7c89730446474e1059acf21264ef633a4
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2016-03-07 12:26:35 -0300

    Fixed invalid middlewares locations

--- api/server.js
+++ api/server.js
@@ -1,16 +1,20 @@
 import express from "express";
 
-import bodyParserMiddleware from "./middlewares/body-parser";
-import errorMiddleware from "./middlewares/error";
+import bodyParser from "./middlewares/body-parser";
+import handleError from "./middlewares/handle-error";
+import handleInvalidResource from "./middlewares/handle-invalid-resource";
+import handleResult from "./middlewares/handle-result";
 
 import routes from "./routes";
 
 const server = express();
 
-server.use(bodyParserMiddleware);
+server.use(bodyParser);
 
 server.use(routes);
 
-server.use(errorMiddleware);
+server.use(handleResult);
+server.use(handleInvalidResource);
+server.use(handleError);
 
 export default server;
