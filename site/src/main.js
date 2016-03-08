19ac4c8210fdfe7e090f7c7b89757316847376d6
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2016-03-08 04:29:40 -0300

    Router should have been imported from react-router and not from react-router-redux

--- site/src/main.js
+++ site/src/main.js
@@ -2,7 +2,7 @@ import "babel-polyfill";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { Router } from "react-router-redux";
+import { Router } from "react-router";
 
 import applicationRoute from "pages/application/route";
 import history from "lib/history";
