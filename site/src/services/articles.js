94cc416d69fc4d59894cd9ca5d6192e694273fb0
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2016-03-08 12:54:27 -0300

    Fixed typo

--- site/src/services/articles.js
+++ site/src/services/articles.js
@@ -2,6 +2,6 @@ import createApiResourceManager from "lib/create-api-resource-manager";
 
 // services are not tested since they are just instances of ApiResourceManager
 export default createApiResourceManager({
-    baseUrl     : __config.apiPath,
+    basePath    : __config.apiPath,
     resourceName: "articles"
 });
