5ac32cd4d0c353c70e4fcb8e947b2f8c810f0998
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2016-03-08 02:56:20 -0300

    Added action: articles fetch

---
+++ site/src/store/actions/articles/fetch.js
@@ -0,0 +1,12 @@
+import articlesService from "services/articles";
+import createAsyncActionCreator from "../lib/create-async-action-creator";
+
+export function articlesFetch(articlesService, articleId) {
+
+    return articlesService.fetch(articleId);
+}
+
+export default createAsyncActionCreator(
+    "ARTICLES:FETCH",
+    articlesFetch.bind(undefined, articlesService)
+);
