3d00cc4b52b1b5a8484cab4c0fbe4d1d97879759
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2016-03-09 12:10:55 -0300

    Article is passed directly in the action payload

--- site/src/store/reducers/articles/handlers/edit.js
+++ site/src/store/reducers/articles/handlers/edit.js
@@ -2,6 +2,6 @@ import articlesEditAction from "store/actions/articles/edit";
 
 export default {
     [ articlesEditAction.TYPE ]: (state, action) => state.merge({
-        inEditionArticle: action.payload.article
+        inEditionArticle: action.payload
     })
 };
