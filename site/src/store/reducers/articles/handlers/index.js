28e7d6b0c62be8ef2e8db0b9d9a414f983607dd4
Mauro Gabriel Titimoli <mgtitimoli@gmail.com>
2016-03-09 12:13:12 -0300

    Added articles end-edition and fetch handlers

--- site/src/store/reducers/articles/handlers/index.js
+++ site/src/store/reducers/articles/handlers/index.js
@@ -1,5 +1,7 @@
 import articlesCreateHandlers from "./create";
 import articlesEditHandlers from "./edit";
+import articlesEndEditionHandlers from "./end-edition";
+import articlesFetchHandlers from "./fetch";
 import articlesFetchAllHandlers from "./fetch-all";
 import articlesUpdateHandlers from "./update";
 
@@ -7,6 +9,8 @@ export default Object.assign(
     {},
     articlesCreateHandlers,
     articlesEditHandlers,
+    articlesEndEditionHandlers,
+    articlesFetchHandlers,
     articlesFetchAllHandlers,
     articlesUpdateHandlers
 );
