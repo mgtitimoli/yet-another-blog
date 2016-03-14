b583d03b57631cb0e33af1b2411a9f3fa3349714
Mauro Titimoli <mgtitimoli@gmail.com>
2016-03-14 01:13:20 -0300

    Removed articles edit and articles end-edition reducers

--- site/src/store/reducers/articles/handlers/edit.js
+++
@@ -1,7 +0,0 @@
-import articlesEditAction from "store/actions/articles/edit";
-
-export default {
-    [ articlesEditAction.TYPE ]: (state, action) => state.merge({
-        inEditionArticle: action.payload
-    })
-};
--- site/src/store/reducers/articles/handlers/end-edition.js
+++
@@ -1,7 +0,0 @@
-import articlesEndEditionAction from "store/actions/articles/end-edition";
-
-export default {
-    [ articlesEndEditionAction.TYPE ]: (state/*, action*/) => state.merge({
-        inEditionArticle: null
-    })
-};
