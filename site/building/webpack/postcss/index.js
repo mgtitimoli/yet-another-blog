fec7b2890c157bc984fe241de291ccf8c59e9485
Mauro Titimoli <mgtitimoli@gmail.com>
2016-03-14 03:52:00 -0300

    Removed no longer used plugins and added css-variables one

--- site/building/webpack/postcss/index.js
+++ site/building/webpack/postcss/index.js
@@ -3,29 +3,19 @@
 var autoPrefixer = require("autoprefixer");
 var calc         = require("postcss-calc");
 var customMedia  = require("postcss-custom-media");
-var conditionals = require("postcss-conditionals");
+var cssVariables = require("postcss-css-variables");
 var reporter     = require("postcss-reporter");
-var mixins       = require("postcss-mixins");
-var nested       = require("postcss-nested");
-var simpleVars   = require("postcss-simple-vars");
-
-var path = require("path");
 
 module.exports = {
     defaults: [
         reporter({ throwError: true }),
-        mixins({
-            mixinsDir: path.join(__dirname, "mixins")
-        }),
-        nested,
-        simpleVars({
+        cssVariables({
             variables: require("./variables")
         }),
         calc,
         autoPrefixer,
         customMedia({
             extensions: require("./extensions")
-        }),
-        conditionals
+        })
     ]
 };
