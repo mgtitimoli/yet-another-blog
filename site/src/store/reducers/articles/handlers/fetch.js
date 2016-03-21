ec3856a516bee5b2152cbe075040a6dc5e5ca464
Mauro Titimoli <mgtitimoli@gmail.com>
2016-03-21 02:54:24 -0300

    Improved articles fetch reducer to better express which state node updates

--- site/src/store/reducers/articles/handlers/fetch.js
+++ site/src/store/reducers/articles/handlers/fetch.js
@@ -3,20 +3,20 @@ import Immutable from "immutable";
 import articlesFetchAction from "store/actions/articles/fetch";
 
 export default {
-    [ articlesFetchAction.TYPE_SUCCEED ]: (state, { payload: article }) => {
+    [ articlesFetchAction.TYPE_SUCCEED ]:
+        (state, { payload: article }) => state.update(
+            "collection",
+            collection => {
 
-        const collection = state.get("collection");
+                const articleIndex = collection.findIndex(
+                    curArticle => curArticle.get("id") === article.id
+                );
 
-        const articleIndex = collection.findIndex(
-            curArticle => curArticle.get("id") === article.id
-        );
+                const immutableArticle = Immutable.fromJS(article);
 
-        const immutableArticle = Immutable.fromJS(article);
-
-        return state.merge({
-            collection: articleIndex >= 0 ?
-                collection.set(articleIndex, immutableArticle) :
-                collection.push(immutableArticle)
-        });
-    }
+                return articleIndex >= 0 ?
+                    collection.set(articleIndex, immutableArticle) :
+                    collection.push(immutableArticle);
+            }
+        )
 };
